e515fd3698931f7f9c3443aa38d29295b0669484
Alikhan <nalikhan@bu.edu>
2019-09-09 20:58:57 -0400

    Edited park

--- outdoors/park.py
+++ outdoors/park.py
@@ -1,3 +1,19 @@
-def draw_park():
-    print("park not found")
+def draw_park(num_trees = 10, num_people = 10):
+    """
+    The function prints symbolic trees and people.
+    It takes as parameters: 'num_trees' - number of trees to print.
+                            'num_people' - number of people to print.
+    Returns None.
+    """
+    if num_trees < 0:
+        raise ValueError('Number of trees can not be negative')
+    for _ in range(num_trees):
+        print('~\|/~')
+        
+    if num_people < 0:
+        raise ValueError('Number of people can not be negative')
+    for _ in range(num_people):
+            print('-_-')
+            print('<|>')
+            print("/ \ ")        
     return
